2930c15bfd8cfc0dd6afdcd47f26e1b82d884281
mycsoft2000 <mycsoft2000@7602cd32-584e-0410-9b18-d167925eb882>
2008-06-19 05:56:55 +0000

    Findbug first

---
+++ FindbugsTools/src/cbg/app/Logger.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cbg.app;
+
+/**
+ *
+ * @author Ma Yichao
+ */
+public class Logger {
+
+    public static boolean isLogging(){
+        return true;
+    }
+    
+    public static void log(String s,String s2){
+        
+    }
+    
+    public static void log(String s){
+        
+    }
+}
